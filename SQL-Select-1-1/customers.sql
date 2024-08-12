816e9b3ea7c2896519d53f210780e34fb2187172
pablobeckg <beckgarreaud@gmail.com>
2024-08-12 15:57:38 +0200

    sql

---
+++ SQL-Select-1-1/customers.sql
@@ -0,0 +1,7 @@
+1) SELECT * FROM Customers;
+2) SELECT CustomerName, City, Country FROM Customers;
+3) SELECT * FROM CUSTOMERS WHERE Country = 'Germany';
+4) SELECT * FROM Customers WHERE City = 'London' AND Country = 'UK';
+5) SELECT * FROM Customers ORDER BY CustomerName ASC;
+6) SELECT TOP 5 * FROM Customers;
+7) SELECT COUNT(*) AS total_customers FROM Customers;
---
+++ SQL-Select-2-1/customers.sql
@@ -0,0 +1,7 @@
+1) SELECT * FROM Customers WHERE CustomerName LIKE 'A%';
+2) SELECT * FROM Customers ORDER BY PostalCode DESC;
+3) SELECT * FROM Customers WHERE Country = 'Mexico' ORDER BY City, CustomerName;
+4) SELECT * FROM Customers WHERE Country IN ('Germany', 'UK', 'France');
+5) SELECT * FROM Products WHERE Unit LIKE '%bottle%';
+6 ) SELECT * FROM Products WHERE Price BETWEEN 20 AND 30;
+7 ) 
